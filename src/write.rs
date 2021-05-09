ac32b8f30b246eb53709a97ba275798990c582de
Alex Low <me@alexlow.dev>
2021-05-09 01:11:06 -0400

    chore: continue with lession 13

--- src/write.rs
+++ src/write.rs
@@ -1,5 +1,25 @@
-use color_eyre::Result;
+use color_eyre::{eyre::WrapErr, Result};
+use edit::{edit_file, Builder};
+use std::io::{Read, Write};
+use std::path::PathBuf;
 
-pub fn write(_title: Option<String>) -> Result<()> {
+const TEMPLATE: &[u8; 2] = b"# ";
+
+pub fn write(
+  garden_path: PathBuf,
+  title: Option<String>,
+) -> Result<()> {
+  let (mut file, filepath) = Builder::new()
+    .suffix(".md")
+    .rand_bytes(5)
+    .tempfile_in(&garden_path)
+    .wrap_err("Failed to create wip file")?
+    .keep()
+    .wrap_err("Failed to keep tempfile")?;
+  file.write_all(TEMPLATE)?;
+  edit_file(filepath)?;
+  let mut contents = String::new();
+  file.read_to_string(&mut contents)?;
+  dbg!(contents);
   todo!()
 }
