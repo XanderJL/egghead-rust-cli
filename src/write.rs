c31285871959592351d9624eff44c05325ae838f
Alex Low <me@alexlow.dev>
2021-05-09 09:46:12 -0400

    chore: fix bugs :(

--- src/write.rs
+++ src/write.rs
@@ -1,14 +1,12 @@
-use color_eyre::{eyre::WrapErr, Result};
+use color_eyre::{eyre::WrapErr, owo_colors::OwoColorize, Result};
 use edit::{edit_file, Builder};
-use std::io::{Read, Write};
+use std::fs;
+use std::io::{Read, Seek, SeekFrom, Write};
 use std::path::PathBuf;
 
 const TEMPLATE: &[u8; 2] = b"# ";
 
-pub fn write(
-  garden_path: PathBuf,
-  title: Option<String>,
-) -> Result<()> {
+pub fn write(garden_path: PathBuf, title: Option<String>) -> Result<()> {
   let (mut file, filepath) = Builder::new()
     .suffix(".md")
     .rand_bytes(5)
@@ -17,9 +15,76 @@ pub fn write(
     .keep()
     .wrap_err("Failed to keep tempfile")?;
   file.write_all(TEMPLATE)?;
-  edit_file(filepath)?;
+  edit_file(&filepath)?;
+
   let mut contents = String::new();
+  file.seek(SeekFrom::Start(0))?;
   file.read_to_string(&mut contents)?;
-  dbg!(contents);
-  todo!()
+
+  let document_title = title.or_else(|| {
+    contents
+      .lines()
+      .find(|v| v.starts_with("# "))
+      .map(|maybe_line| maybe_line.trim_start_matches("# ").to_string())
+  });
+
+  let filename = match document_title {
+    Some(raw_title) => confirm_filename(&raw_title),
+    None => ask_for_filename(),
+  }?;
+  let mut i: usize = 0;
+  loop {
+    let dest_filename = format!(
+      "{}{}",
+      filename,
+      if i == 0 {
+        "".to_string()
+      } else {
+        i.to_string()
+      }
+    );
+    let mut dest = garden_path.join(dest_filename);
+    dest.set_extension("md");
+    if dest.exists() {
+      i = i + 1;
+    } else {
+      fs::rename(filepath, &dest)?;
+      break;
+    }
+  }
+  Ok(())
+}
+
+fn ask_for_filename() -> Result<String> {
+  rprompt::prompt_reply_stderr(&format!(
+    "{}",
+    "\
+    Enter filename
+    >"
+    .blue()
+    .bold(),
+  ))
+  .wrap_err("Failed to get filename")
+  .map(|title| slug::slugify(title))
+}
+
+fn confirm_filename(raw_title: &str) -> Result<String> {
+  loop {
+    let result = rprompt::prompt_reply_stderr(&format!(
+      "\
+      {}{}
+      Do you want a different title? (y/N): ",
+      "current title:".green().bold(),
+      raw_title,
+    ))
+    .wrap_err("Failed to get input for y/n question")?;
+
+    match result.as_str() {
+      "y" | "Y" => break ask_for_filename(),
+      "n" | "N" | "" => {
+        break Ok(slug::slugify(raw_title));
+      }
+      _ => {}
+    }
+  }
 }
--- tests/integrations.rs
+++ tests/integrations.rs
@@ -1,5 +1,25 @@
 use assert_cmd::Command;
+use assert_fs::prelude::*;
 use color_eyre::eyre::Result;
+use predicates::prelude::*;
+
+fn setup_command() -> (Command, assert_fs::TempDir) {
+  let temp_dir = assert_fs::TempDir::new().unwrap();
+
+  let mut cmd = Command::cargo_bin("garden").unwrap();
+  let fake_editor_path = std::env::current_dir()
+    .expect("expect to be in a dir")
+    .join("tests")
+    .join("fake_editor.sh");
+  if !fake_editor_path.exists() {
+    panic!("fake editor shell script could not be found")
+  }
+
+  cmd
+    .env("EDITOR", fake_editor_path.into_os_string())
+    .env("GARDEN_PATH", temp_dir.path());
+  (cmd, temp_dir)
+}
 
 #[test]
 /// Make sure help runs. This indicates the binary works
@@ -20,10 +40,35 @@ fn test_write_help() -> Result<()> {
 }
 
 #[test]
-/// Make sure help runs. This indicates the binary works
-fn test_write() -> Result<()> {
-  let mut cmd = Command::cargo_bin("garden")?;
-  let assert = cmd.arg("write").assert();
-  assert.success().stderr("");
-  Ok(())
+fn test_write_with_title() {
+  let (mut cmd, temp_dir) = setup_command();
+  let assert = cmd
+    .arg("write")
+    .arg("-t")
+    .arg("atitle")
+    .write_stdin("N\n".as_bytes())
+    .assert();
+
+  assert.success();
+
+  temp_dir
+    .child("atitle.md")
+    .assert(predicate::path::exists());
+}
+
+#[test]
+fn test_write_with_written_title() {
+  let (mut cmd, temp_dir) = setup_command();
+  let assert = cmd
+    .arg("write")
+    .arg("-t")
+    .arg("atitle")
+    .write_stdin("N\n".as_bytes())
+    .assert();
+
+  assert.success();
+
+  temp_dir
+    .child("testing.md")
+    .assert(predicate::path::exists());
 }
